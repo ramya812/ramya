a58f56a39b55e5d5b7b418d7cc6ca705f6990e3f
tejarohan <ramyakodali812@gmail.com>
2016-07-27 12:02:12 -0500

    added Google mail folder

---
+++ GoogleMailLogin/LogIntestCase[Conflict].java
@@ -0,0 +1,38 @@
+package GoogleMailLogin;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LogIntestCase {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver driver=new FirefoxDriver();
+        driver.get("https://www.gmail.com");
+        Thread.sleep(3000);
+        WebElement EmailId=driver.findElement(By.id("Email"));
+        EmailId.sendKeys("sampletest98765");
+        Thread.sleep(2000);
+        driver.findElement(By.id("next")).click();
+        Thread.sleep(3000);
+       
+        driver.findElement(By.id("Passwd")).sendKeys("Asdf@420");
+        Thread.sleep(2000);
+        
+        driver.findElement(By.id("signIn")).click();
+        Thread.sleep(4000);
+        driver.findElement(By.id(":j")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.className("J-N-Jz")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.id(":bq")).click();
+        Thread.sleep(2000);
+        
+        
+        
+
+	}
+
+}
